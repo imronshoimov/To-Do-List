0d078f54f342ee82060e67deb8ceb47f4b75d512
imronshoimov <77952210+imronshoimov@users.noreply.github.com>
2021-04-01 23:10:48 +0500

    Create elements

---
+++ main.js
@@ -0,0 +1,5 @@
+let formElement = document.querySelector(".todo-list-form")
+let inputElement = document.querySelector(".todo-list-input")
+let addElement = document.querySelector(".todo-list-btn")
+
+let listElement = document.querySelector(".todo-list")
