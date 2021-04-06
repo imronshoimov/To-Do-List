f34c662fed74c869ebbc575252d7db1990b2d23a
imronshoimov <77952210+imronshoimov@users.noreply.github.com>
2021-04-06 15:11:15 +0500

    Correct js mistakes

--- main.js
+++ main.js
@@ -7,54 +7,54 @@ let todo = []
 
 formElement.addEventListener("submit", evt => {
    evt.preventDefault()
-   let newLiElement = document.createElement("li")
-   newLiElement.classList.add("todo-items")
-   let newPElement = document.createElement("p")
-   newPElement.classList.add("todo-text")
-   let newbtnElement = document.createElement("button")
-   newbtnElement.classList.add("remove-btn")
-   newbtnElement.textContent = "x"
-
-   if (inputElement.value == "") {
-      newLiElement.textContent = undefined
-      newbtnElement.style.display = "none"
-      newLiElement.style.borderBottom = "none"
-      newLiElement.style.padding = 0
-      newLiElement.style.margin = 0
-      newPElement.style.margin = 0
-      newPElement.style.padding = 0
-      alert("please write smth")
-   } else {
-      newPElement.textContent = inputElement.value
+   if (!inputElement.value) return alert("Please wrtie smth")
+   let newTodoObject = {
+      name: inputElement.value
    }
-   newLiElement.appendChild(newPElement)
-   newLiElement.appendChild(newbtnElement)
-   listElement.appendChild(newLiElement)
+   todo.push(newTodoObject)
+   renderItems(todo)
 
-   newbtnElement.addEventListener("click", e => {
-      newLiElement.textContent = undefined
-      newLiElement.style.textDecoration = "none"
-      newLiElement.style.padding = 0
-      newLiElement.style.margin = 0
-   })
-
-   let toggle = true
-
-   newPElement.addEventListener("click", evt => {
-      if (toggle) {
-         newPElement.style.textDecoration = "line-through"
-         toggle = false
-      } else {
-         newPElement.style.textDecoration = "none"
-         toggle = true
-      }
-   })
-
-
-   newLiElement.addEventListener("copy", e => {
-      alert("copied")
-   })
-
-   formElement.reset()
+   evt.target.reset()
    formElement.focus()
 })
+
+function renderItems(array) {
+   listElement.textContent = ''
+   array.forEach((e, i) => {
+      // let idElement = document.createElement("p")
+      // idElement.textContent = 1 + i
+      let newLiElement = document.createElement("li")
+      newLiElement.classList.add("todo-items")
+      let newPElement = document.createElement("p")
+      newPElement.classList.add("todo-text")
+      newPElement.textContent = e.name
+      let newbtnElement = document.createElement("button")
+      newbtnElement.classList.add("remove-btn")
+      newbtnElement.textContent = "x"
+
+      let toggle = true
+      newPElement.addEventListener("click", evt => {
+         if (toggle) {
+            newPElement.style.textDecoration = "line-through"
+            toggle = false
+         } else {
+            newPElement.style.textDecoration = "none"
+            toggle = true
+         }
+      })
+
+      newbtnElement.addEventListener('click', evt => {
+         todo.splice(i, 1)
+         renderItems(todo)
+      })
+
+      newLiElement.addEventListener("copy", e => {
+         alert("copied")
+      })
+
+      newLiElement.appendChild(newPElement)
+      // newLiElement.appendChild(idElement)
+      newLiElement.appendChild(newbtnElement)
+      listElement.appendChild(newLiElement)
+   })
+}
